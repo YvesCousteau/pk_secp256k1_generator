98d720e4323cf3bd3f3c3a650dc2c036c0c66278
Your Name <you@example.com>
2024-11-21 14:10:06 +0100

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use rand::rngs::OsRng;
 use secp256k1::Secp256k1;
 
 fn main() -> () {
     let secp = Secp256k1::new();
-    let mut rng = rand::thread_rng();
+    let mut rng = OsRng; // rng is a number between 0 <= k <= 2^256
     let (secret_key, _) = secp.generate_keypair(&mut rng);
     print!("{}\n", secret_key.display_secret().to_string());
 }
